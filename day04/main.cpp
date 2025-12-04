bd7eeae7d4e90f87149060d72b027b141db1c922
Daniel Kramer <drkspace@gmail.com>
2025-12-04 00:12:30 -0500

    Day 04 solution

--- day04/main.cpp
+++ day04/main.cpp
@@ -1,31 +1,151 @@
 
 #include <print>
 #include "../utils/utils.h"
+#include "../utils/easyTimer.hpp"
 #include <cstdio>
 #include <cmath>
 
 auto getInput(const std::string& fp)
 {
-    const auto lines = read_file(fp);
-    std::vector<std::vector<u64>> out;
-
-    for (const auto& line: lines)
-    {
-
-    }
-
-    return out;
+    return read_file(fp);
 }
 
 int main(const int argc, char* argv[])
 {
+    easyTimer<std::chrono::milliseconds> _;
     if (!(argc == 2 || argc == 3))
     {
         std::println(stderr, "Invalid Args");
         return 1;
     }
-    const auto inp = getInput(argv[1]);
+    auto inp = getInput(argv[1]);
 
+    {
+        int tot = 0;
+        for (size_t i = 0; i< inp.size(); i++)
+        {
+            for (size_t j = 0; j<inp[i].size(); j++)
+            {
+                if (inp[i][j] == '.')
+                {
+                    continue;
+                }
+                int c = 0;
+                if (i>0)
+                {
+                    if (j > 0)
+                    {
+                        c += inp[i-1][j-1] == '@';
+                    }
+                    if (j < inp.size()-1)
+                    {
+                        c += inp[i-1][j+1] == '@';
+                    }
+                    c += inp[i-1][j] == '@';
+                }
+
+                if (j > 0)
+                {
+                    c += inp[i][j-1] == '@';
+                }
+                if (j < inp.size()-1)
+                {
+                    c += inp[i][j+1] == '@';
+                }
+                if (i<inp.size()-1)
+                {
+                    if (j > 0)
+                    {
+                        c += inp[i+1][j-1] == '@';
+                    }
+                    if (j < inp.size()-1)
+                    {
+                        c += inp[i+1][j+1] == '@';
+                    }
+                    c += inp[i+1][j] == '@';
+                }
+                if (c<4)
+                {
+                    tot++;
+                }
+            }
+        }
+        std::println("Part 1 {}", tot);
+    }
+
+    {
+
+        int tot = 0;
+        int prev = 1;
+        int cur = 0;
+        while (prev != cur)
+        {
+            prev = cur;
+            int this_count = 0;
+            for (size_t i = 0; i< inp.size(); i++)
+            {
+                for (size_t j = 0; j<inp[i].size(); j++)
+                {
+                    if (inp[i][j] == '.')
+                    {
+                        continue;
+                    }
+                    int c = 0;
+                    if (i>0)
+                    {
+                        if (j > 0)
+                        {
+                            c += inp[i-1][j-1] == '@';
+                        }
+                        if (j < inp.size()-1)
+                        {
+                            c += inp[i-1][j+1] == '@';
+                        }
+                        c += inp[i-1][j] == '@';
+                    }
+
+                    if (j > 0)
+                    {
+                        c += inp[i][j-1] == '@';
+                    }
+                    if (j < inp.size()-1)
+                    {
+                        c += inp[i][j+1] == '@';
+                    }
+                    if (i<inp.size()-1)
+                    {
+                        if (j > 0)
+                        {
+                            c += inp[i+1][j-1] == '@';
+                        }
+                        if (j < inp.size()-1)
+                        {
+                            c += inp[i+1][j+1] == '@';
+                        }
+                        c += inp[i+1][j] == '@';
+                    }
+                    if (c<4)
+                    {
+                        this_count++;
+                        inp[i][j] = '.';
+                    }
+                }
+            }
+
+            tot += this_count;
+
+            cur = 0;
+            for (const auto& row: inp)
+            {
+                for (const auto& c: row)
+                {
+                    cur += c == '@';
+                }
+            }
+
+        }
+        std::println("Part 2 {}", tot);
+    }
 
     return 0;
 }
